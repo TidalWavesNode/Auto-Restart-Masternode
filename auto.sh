653e70f45e0f55ae1157bf437ae6008803837f2a
Tidal Waves <33072338+TidalWavesNode@users.noreply.github.com>
2018-10-01 14:05:05 -0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-cat > mn_auto.txt <<- "EOF"
-test
-more contents
+function configure_script() {
+  cat << EOF > ~/wagerr-2.0.0/bin
+#!/bin/bash
+SERVICE=’wagerrd’
+
+if ps ax | grep -v grep | grep $SERVICE > /dev/null
+then
+echo “Masternode is running! Yay!”
+else
+~/wagerr-2.0.0/bin/./wagerrd
+fi
 EOF
+
