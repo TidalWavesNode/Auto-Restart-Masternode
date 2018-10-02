6d4d7ce71f31b54a431e0ca53439599550c29a0b
Tidal Waves <33072338+TidalWavesNode@users.noreply.github.com>
2018-10-02 07:14:51 -0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -15,4 +15,4 @@ EOT
 
 chmod 744 mn_auto.sh
 
-crontab -l | { cat; echo "*/1 * * * * ~/wagerr-2.0.0/bin/mn_auto.sh >> ~/wagerr-2.0.0/bin/cronlog/auto.log 2>&1 #logs output to auto.log"; } | crontab -​
+crontab -l | { cat; echo "*/1 * * * * /root/wagerr-2.0.0/bin/mn_auto.sh >> /root/wagerr-2.0.0/bin/cronlog/auto.log 2>&1 #logs output to auto.log"; } | crontab -​
