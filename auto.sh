7fb5a275bc32c7bf629ac5df024df36bcc81ef3d
Tidal Waves <33072338+TidalWavesNode@users.noreply.github.com>
2018-10-02 06:12:04 -0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-​
+
 cd ~/wagerr-2.0.0/bin
 mkdir cronlog
 
-cat <<EOF > mn_auto.sh
+cat <<EOT > mn_auto.sh
 #!/bin/bash
 SERVICE='wagerrd'
 if ps ax | grep -v grep | grep $SERVICE > /dev/null
@@ -12,8 +12,8 @@ echo “Masternode is running! Yay!”
 else
 ~/wagerr-2.0.0/bin/./wagerrd
 fi
-EOF
+EOT
 
 chmod 744 mn_auto.sh
-​
+
 crontab -l | { cat; echo "*/1 * * * * ~/wagerr-2.0.0/bin/mn_auto.sh >> ~/wagerr-2.0.0/bin/cronlog/auto.log 2>&1 #logs output to auto.log"; } | crontab -​
