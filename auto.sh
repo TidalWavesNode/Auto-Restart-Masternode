9ea31c0729f754fdb4bec57b4b7223f34b1a97c1
Tidal Waves <33072338+TidalWavesNode@users.noreply.github.com>
2018-10-02 06:16:55 -0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-cd ~/wagerr-2.0.0/bin
-mkdir cronlog
+crontab -l | { cat; echo "*/1 * * * * ~/wagerr-2.0.0/bin/mn_auto.sh >> ~/wagerr-2.0.0/bin/cronlog/auto.log 2>&1 #logs output to auto.log"; } | crontab -​
