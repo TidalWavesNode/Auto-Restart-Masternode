3b8b5d1d25088225daaf3ca09dc8879e41e2cff8
Tidal Waves <33072338+TidalWavesNode@users.noreply.github.com>
2021-08-18 11:37:14 -0700

    Update StorX-Node_AUTO-Check.sh

--- StorX-Node_AUTO-Check.sh
+++ StorX-Node_AUTO-Check.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-read -p "Do you want to create the n_auto script? (Y/n) " -n 1 -r
+read -p "Do you want to create the srx_auto_check script? (Y/n) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 
 cd ~/StorX-Node
 
-cat <<EOT > n_auto.sh
+cat <<EOT > srx_auto_check.sh
 #!/bin/bash
 SERVICE='node'
 if ps ax | grep -v grep | grep $SERVICE > /dev/null
@@ -18,7 +18,7 @@ sudo docker-compose -f docker-services.yml up -d
 fi
 EOT
 
-chmod 744 n_auto.sh
+chmod 744 srx_auto_check.sh
 fi
 
 read -p "Do you want to add the required cron task? (Y/n) " -n 1 -r
@@ -26,7 +26,7 @@ echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 
-crontab -l | { cat; echo "*/15 * * * * /root/StorX-Node/bin/n_auto.sh >> /root/StorX-Node/bin/cronlog/auto.log 2>&1 #logs output to auto.log"; } | crontab - l
+crontab -l | { cat; echo "*/15 * * * * /root/StorX-Node/srx_auto_check.sh >> /root/StorX-Node/cronlog/auto.log 2>&1 #logs output to auto.log"; } | crontab - l
 
 mkdir cronlog
 fi
